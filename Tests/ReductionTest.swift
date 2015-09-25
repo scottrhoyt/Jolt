eb5f45be79b64f9d4a421af66bfd1bb5f2ed81c7
Scott Hoyt <scott@wildonion.io>
2015-09-24 20:49:39 -0700

    Finished statistics tests and added override for accuracy to pass sum.

--- Tests/ReductionTest.swift
+++ Tests/ReductionTest.swift
@@ -14,14 +14,21 @@ protocol ReductionTest : RandomDataTest {
     typealias OperandType : FloatingPointAccuracy
     
     func measureAndValidateMappedFunctionWithAccuracy(reduction: ([OperandType]) -> (OperandType), mapped: ([OperandType]) -> (OperandType))
+    func measureAndValidateMappedFunctionWithAccuracy(reduction: ([OperandType]) -> (OperandType), mapped: ([OperandType]) -> (OperandType), accuracy: OperandType)
     
 }
 
 extension ReductionTest where Self : XCTestCase {
     
     func measureAndValidateMappedFunctionWithAccuracy(reduction: ([OperandType]) -> (OperandType), mapped: ([OperandType]) -> (OperandType)) {
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: mapped, accuracy: OperandType.accuracy)
+    }
+    
+    func measureAndValidateMappedFunctionWithAccuracy(reduction: ([OperandType]) -> (OperandType), mapped: ([OperandType]) -> (OperandType), accuracy: OperandType)
+    {
         let values = rands(SurgeTestCountMedium)
-        measureAndValidateMappedFunctionWithAccuracy(values, reduction: reduction, mapped: mapped, accuracy: OperandType.accuracy)
+        measureAndValidateMappedFunctionWithAccuracy(values, reduction: reduction, mapped: mapped, accuracy: accuracy)
+
     }
     
 }
--- Tests/StatisticsTests.swift
+++ Tests/StatisticsTests.swift
@@ -16,6 +16,21 @@ class DoubleStatisticsTests: XCTestCase, ReductionTest {
         let reduction = { (data: [OperandType]) in return data.reduce(OperandType(0), combine: { $0 + $1 }) }
         measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.sum)
     }
+    
+    func test_min() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType.infinity, combine: { min($0, $1) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.min)
+    }
+    
+    func test_max() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType(-1e32), combine: { max($0, $1) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.max)
+    }
+    
+    func test_mean() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType(0), combine: { $0 + $1 / OperandType(data.count) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.mean)
+    }
 
 }
 
@@ -25,7 +40,22 @@ class FloatStatisticsTests: XCTestCase, ReductionTest {
     
     func test_sum() {
         let reduction = { (data: [OperandType]) in return data.reduce(OperandType(0), combine: { $0 + $1 }) }
-        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.sum)
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.sum, accuracy: 0.001)
+    }
+    
+    func test_min() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType.infinity, combine: { min($0, $1) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.min)
+    }
+    
+    func test_max() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType(-1e32), combine: { max($0, $1) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.max)
+    }
+    
+    func test_mean() {
+        let reduction = { (data: [OperandType]) in return data.reduce(OperandType(0), combine: { $0 + $1 / OperandType(data.count) }) }
+        measureAndValidateMappedFunctionWithAccuracy(reduction, mapped: OperandType.mean)
     }
     
 }
